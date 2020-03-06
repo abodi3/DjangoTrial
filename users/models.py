142549b3855c3755a10f38d83acd7192add5ac9c
Albert Offei <abodi3@Alberts-MacBook-Air-1958.local>
2020-03-06 13:42:19 +0200

    changes

--- users/models.py
+++ users/models.py
@@ -16,12 +16,11 @@ class Profile(models.Model):
 		return f'{self.user.username} Profile'
 
 
-	def save(self):
-		super().save()
-
+	def save(self, *args, **kwargs):
+		super(Profile, self).save(*args, **kwargs)
 		img = Image.open(self.image.path)
 
 		if img.height > 300 or img.width > 300:
-			output_size = (300, 300)
+			output_size = (300,300)
 			img.thumbnail(output_size)
 			img.save(self.image.path)
